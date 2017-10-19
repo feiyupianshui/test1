876737b1a64feb55598bffe0af8e1e3e11a2b073
feiyupianshui <ed273030@gmail.com>
2017-10-19 13:16:13 +0800

    commit 1019

--- pipelines.py
+++ pipelines.py
@@ -5,7 +5,7 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 from pymongo import MongoClient
-import datatime
+import datetime
 
 class Test1Pipeline(object):
 
@@ -15,20 +15,18 @@ class Test1Pipeline(object):
         self.save = db['drama']
 
     def process_item(self, item, spider):
-        if isinstance(item, RadiowaveItem):
-            dramaid = int(item['dramaid'])
-            dramaname = item['dramaname']
-            category = item['category']
-            imgurl = item['imgurl']
-            dramapage = item['dramapage']
-            post = {
-                '_id': dramaid,
-                '剧名': dramaname,
-                '类型': category,
-                '海报链接': imgurl,
-                '详情页面': dramapage,
-                '储存时间': datetime.datetime.now()
-            }
-            self.save.insert_one(post)
-
+        dramaid = int(item['dramaid'])
+        dramaname = item['dramaname']
+        category = item['category']
+        imgurl = item['imgurl']
+        dramapage = item['dramapage']
+        post = {
+            '_id': dramaid,
+            '剧名': dramaname,
+            '类型': category,
+            '海报链接': imgurl,
+            '详情页面': dramapage,
+            '储存时间': datetime.datetime.now()
+        }
+        self.save.insert_one(post)
         return item
